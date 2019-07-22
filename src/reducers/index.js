14552b4032a8312e3563b5144da8353d1b377d23
another_xq <anohter_xiaoqiang@163.com>
2019-07-22 17:01:41 +0800

    commit because I want to commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,14 @@ import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilters from './visibilityFilters'
 
-export default combineReducers({
+// export default combineReducers({
+//   todos,
+//   visibilityFilter
+// })
+
+const rootReducer = combineReducers({
   todos,
   visibilityFilters
 })
+
+export default rootReducer
